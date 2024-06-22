421a254de14f9097ba887c5e572c2a1d1be08fa3
saepulfariz <saepulfariz108@gmail.com>
2024-06-22 22:06:33 +0700

    success on off led

--- HTML.h
+++ HTML.h
@@ -10,8 +10,42 @@ const char index_html[] PROGMEM = R"rawliteral(
 </head>
 <body>
     <h3 style="text-align: center;">BLINK WEB - Saepulfariz</h3>
+    <a href="/on" >ON</a> | <a href="/off" >OFF</a>
+</body>
+</html>
+)rawliteral";
+
+const char on_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLINK WEB - Saepulfariz</title>
+
+    
+</head>
+<body>
+    <h3 style="text-align: center;">BLINK WEB(ON) - Saepulfariz</h3>
+    <a href="/on" >ON</a> | <a href="/off" >OFF</a>
 </body>
 </html>
 )rawliteral";
 
 
+const char off_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLINK WEB - Saepulfariz</title>
+
+    
+</head>
+<body>
+    <h3 style="text-align: center;">BLINK WEB(OFF) - Saepulfariz</h3>
+    <a href="/on" >ON</a> | <a href="/off" >OFF</a>
+</body>
+</html>
+)rawliteral";
--- esp8266_blink_web.ino
+++ esp8266_blink_web.ino
@@ -16,6 +16,16 @@ void handleRoot(){
   server.send(200, "text/html", index_html);
 }
 
+void handleOn(){
+  digitalWrite(LED_BUILTIN, HIGH);
+  server.send(200, "text/html", on_html);
+}
+
+void handleOff(){
+  digitalWrite(LED_BUILTIN, LOW);
+  server.send(200, "text/html", off_html);
+}
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
 
@@ -28,7 +38,8 @@ void setup() {
   Serial.println(IP);
 
   server.on("/", handleRoot); // Routine untuk menghandle homepage
-  // server.on("/action_page", handleForm);
+  server.on("/on", handleOn);
+  server.on("/off", handleOff);
 
   server.begin(); // Mulai server
   delay(1000);
@@ -36,9 +47,9 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);                      // Wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+  // digitalWrite(LED_BUILTIN, LOW);
+  //delay(1000);                      // Wait for a second
+  //digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  //delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
   server.handleClient();
 }
