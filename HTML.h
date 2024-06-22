d03ff1beab49560da25f555fca18174f6903d8b2
saepulfariz <saepulfariz108@gmail.com>
2024-06-22 21:46:45 +0700

    init project

---
+++ HTML.h
@@ -0,0 +1,17 @@
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLINK WEB - Saepulfariz</title>
+
+    
+</head>
+<body>
+    <h3 style="text-align: center;">BLINK WEB - Saepulfariz</h3>
+</body>
+</html>
+)rawliteral";
+
+
---
+++ esp8266_blink_web.ino
@@ -0,0 +1,44 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include "HTML.h"
+
+// Konfigurasi WiFi
+const char *ssid = "saepulfariz-device";
+const char *password = "";
+
+String ssidNew = "", passNew;
+
+ESP8266WebServer server(80);
+
+//== Rountin dieksekusi saat browser dibuka / page dipanggil ==//
+void handleRoot(){
+  server.send(200, "text/html", index_html);
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
+
+  Serial.begin(115200);
+  delay(1000);
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.println("");
+  Serial.print("IP Address: ");
+  Serial.println(IP);
+
+  server.on("/", handleRoot); // Routine untuk menghandle homepage
+  // server.on("/action_page", handleForm);
+
+  server.begin(); // Mulai server
+  delay(1000);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+  server.handleClient();
+}
